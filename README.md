83dab3716a314b371e2673d1968769e047896e0f
Daniel <dev.dan.horrigan@gmail.com>
2022-06-17 09:57:29 +0100

    Added support for converting from hex to decimal

--- README.md
+++ README.md
@@ -7,8 +7,43 @@ to decimal.
 
 ```shell
 # convert decimal to hexadecimal:
-python3 pynumsc.py d h 42
+$ python3 pynumsc.py d h 42
 2A
 
 # binary to decimal
+$ python3 pynumsc.py bin dec 10011
+19
+
+# octal to decimal
+$ python3 pynumsc.py octal decimal 17
+15
+
+$ python pynumsc.py -h
+usage: pynumsc.py [-h] source target num
+
+A Simple Command Line Number System Converter
+
+positional arguments:
+  source      number system to convert from
+  target      number system to convert to
+  num         the number to convert
+
+optional arguments:
+  -h, --help  show this help message and exit
 ```
+
+If `source` is `decimal`, `dec`, or `d`, the following `target` number systems are available:
+
+- Binary: `binary`, `bin`, or `b`
+
+- Octal: `octal`, `oct`, or `o`
+
+- Hexadecimal: `hexadecimal`, `hex`, or `h`
+
+If `target` is `decimal`, `dec`, or `d`, the following `source` number systems are available:
+
+- Binary: `binary`, `bin`, or `b`
+
+- Octal: `octal`, `oct`, or `o`
+
+- Hexadecimal: `hexadecimal`, `hex`, or `h`
--- src/pynumsc.py
+++ src/pynumsc.py
@@ -4,42 +4,62 @@ import argparse
 from sys import exit
 
 def cli():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="A Simple Command Line Number System Converter")
     parser.add_argument("source", help="number system to convert from")
     parser.add_argument("target", help="number system to convert to")
-    parser.add_argument("num", help="the number to convert", type=int)
+    parser.add_argument("num", help="the number to convert")
     args = parser.parse_args()
-
-    n = args.num
-    rem = 0
-    div = 0
-    remainder_arr = []
-    hex_letters = ['A', 'B', 'C', 'D', 'E', 'F']
     
+    hex_letters = ['A', 'B', 'C', 'D', 'E', 'F']
+
     if args.source == "d" or args.source == "dec" or args.source == "decimal":
-        pass
-    elif args.source == "b" or args.source == "bin" or args.source == "binary":
-        pass
+        n = int(args.num)
+        rem = 0
+        div = 0
+        remainder_arr = []
 
-    if args.target == "b" or args.target == "bin" or args.target == "binary":
-        div = 2
-    elif args.target == "h" or args.target == "hex" or args.target == "hexadecimal":
-        div = 16
-    elif args.target == "o" or args.target == "oct" or args.target == "octal":
-        div = 8
-    else:
-        print(f"Invalid target number system: {args.target}")
-        exit(1)
+        if args.target == "b" or args.target == "bin" or args.target == "binary":
+            div = 2
+        elif args.target == "h" or args.target == "hex" or args.target == "hexadecimal":
+            div = 16
+        elif args.target == "o" or args.target == "oct" or args.target == "octal":
+            div = 8
+        else:
+            print(f"Invalid target number system: {args.target}")
+            exit(1)
 
-    while n != 0:
-        rem = n % div
-        if rem > 9:
-            rem = hex_letters[rem % 10] 
-        remainder_arr.append(rem)
-        n //= div
+        while n != 0:
+            rem = n % div
+            if rem > 9:
+                rem = hex_letters[rem % 10] 
+            remainder_arr.append(rem)
+            n //= div
 
-    conv_str = ''.join(map(str, remainder_arr[::-1]))
-    print(conv_str)
+        conv_str = ''.join(map(str, remainder_arr[::-1]))
+        print(conv_str)
+    elif args.target == "d" or args.target == "dec" or args.target == "decimal":
+        decimal_num = 0
+        base = 0
+        digit = 0
+
+        if args.source == "b" or args.source == "bin" or args.source == "binary":
+            base = 2
+        elif args.source == "h" or args.source == "hex" or args.source == "hexadecimal":
+            base = 16
+        elif args.source == "o" or args.source == "oct" or args.source == "octal":
+            base = 8
+        else:
+            print(f"Invalid source number system: {args.source}")
+            exit(1)
+        
+        for i, c in enumerate(args.num[::-1]):
+            if c in hex_letters:
+                digit = hex_letters.index(c) + 10
+            else:
+                digit = int(c)
+            decimal_num += base**i * digit
+
+        print(decimal_num)
 
 
 if __name__ == "__main__":
